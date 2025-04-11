46238dfd0095047d9a20a6903277a7194e884767
jhonbellaizac <janerb123@gmail.com>
2025-04-10 23:18:46 -0500

    cree los atributos de la clase terrestre y la interface de motor

--- PracticaInterfaces/src/practicainterfaces/Acuatico.java
+++ PracticaInterfaces/src/practicainterfaces/Acuatico.java
@@ -1,4 +1,27 @@
 package practicainterfaces;
 public class Acuatico extends Vehiculo implements Vela{
     
+    private String tipo;
+    private int cantidadPasajeros;
+
+    public Acuatico(String tipo, int cantidadPasajeros, int velocidadActual, int velocidadMaxima) {
+        super(velocidadActual, velocidadMaxima);
+        this.tipo = tipo;
+        this.cantidadPasajeros = cantidadPasajeros;
+    }
+public int recomendarVelocidad (int velocidadViento){
+    if(velocidadViento > 80){
+        System.out.println("Viento muy fuerte, no se recomienda navegar");
+        velocidadActual = 0;
+        return  0;
+    }else if (velocidadViento < 10){
+        System.out.println("Viento muy debil, no se recomienda navegar");
+        return 0;
+    }else {
+        System.out.println("Condiciones adecuadas para navegar");
+        return velocidadActual;
+    }
+}
+     
+    
 }
--- PracticaInterfaces/src/practicainterfaces/Motor.java
+++ PracticaInterfaces/src/practicainterfaces/Motor.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package practicainterfaces;
 
@@ -8,6 +8,6 @@ package practicainterfaces;
  *
  * @author Jhon Bellaizac
  */
-public class Motor {
+public interface Motor {
     
 }
